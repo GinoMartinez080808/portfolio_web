afe384195088c744c7e34976247ce119164ca5c4
GinoMartinez080808 <130927080+GinoMartinez080808@users.noreply.github.com>
2025-04-07 12:55:34 -0400

    agregado header main y los componentes principales de la estructura del portafolio

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  name = 'Gino Martínez';
+  email = 'ginomartinez690@gmail.com';
+  phone = '+58 0412-2698503';
+  address = 'Cabudare, Lara ,Venezuela';
+  
+}
